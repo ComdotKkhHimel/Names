3c65de1a88926303ffa265c648f41161ae007a8b
ComdotKkhHimel <kkhappcreators@gmail.com>
2025-04-30 23:55:49 +0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-async function generateNames(gender) {
-  const response = await fetch(`${gender}_names.txt`);
-  const text = await response.text();
-  const names = text.trim().split('\n');
-  const shuffled = names.sort(() => 0.5 - Math.random());
-  const selected = shuffled.slice(0, 100000);
-  document.getElementById('output').textContent = selected.join('\n');
-}
